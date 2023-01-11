90c98d038e979873fb19f7e954abce6c9e952e6f
Ray <rguo1010@uw.edu>
2023-01-11 15:20:18 -0800

    Added runaway bot package (not done)

---
+++ src/DB_base_runaway/Launcher.java
@@ -0,0 +1,177 @@
+package DB_base;
+
+import battlecode.common.*;
+
+public strictfp class Launcher {
+
+    public enum LauncherState {
+        Combat, Pursuing, Exploring
+    }
+
+    static RobotInfo[] enemies;
+
+    static WellInfo[]wells;
+    static boolean initialized = false;
+
+    static int combatCD = 0;
+
+    static final Direction[] directions = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
+    };
+
+    static LauncherState state;
+
+    static void run(RobotController rc) throws GameActionException {
+        if(!initialized){
+            onUnitInit(rc); // first time starting the bot, do some setup
+            initialized = true;
+        }
+
+        onTurnStart(rc); // cleanup for when the turn starts
+
+        // interpret overall macro state
+        readComms(rc);
+
+        // sense part
+        sense(rc);
+
+        selectState(rc);
+
+
+
+        //select action based on state
+        switch (state){
+            case Combat:
+                combat(rc);
+                break;
+            case Pursuing:
+                pursue(rc);
+                break;
+            case Exploring:
+                explore(rc);
+                break;
+        }
+
+
+    }
+
+    static void onUnitInit(RobotController rc) throws GameActionException{
+        state = LauncherState.Exploring;
+    }
+
+    static void onTurnStart(RobotController rc) throws GameActionException{
+        combatCD--;
+    }
+
+    static void readComms(RobotController rc)throws GameActionException{
+
+    }
+
+    static void sense(RobotController rc) throws GameActionException{
+        enemies = rc.senseNearbyRobots(RobotType.LAUNCHER.visionRadiusSquared, rc.getTeam().opponent());
+        wells = rc.senseNearbyWells();
+    }
+
+    static void selectState(RobotController rc) throws GameActionException{
+        // select combat state if you see an enemy robot that is not HQ
+        // combatCD will be necessary in the future for avoiding high-cd multiplier squares
+        if(enemies.length > 0){
+            int i = 0;
+            while(i< enemies.length && enemies[i].getType() == RobotType.HEADQUARTERS){
+                i++;
+            }
+            if(i != enemies.length){
+                combatCD =1;
+                state = LauncherState.Combat;
+                rc.setIndicatorString("inCombat");
+            } else{
+                state = LauncherState.Exploring;
+            }
+
+        }
+        // otherwise explore
+        if(state == LauncherState.Combat && combatCD <=0){
+            state = LauncherState.Exploring;
+            return;
+        }
+
+
+    }
+
+    static void combat(RobotController rc) throws GameActionException{
+        RobotInfo attackRobot = findAttack(rc);
+        if(attackRobot!=null){
+            MapLocation attackLoc = attackRobot.getLocation();
+            if(rc.canAttack(attackLoc)){
+                rc.attack(attackLoc);
+            }
+
+
+            // chase after attacked person
+            if(rc.isMovementReady() && rc.getLocation().distanceSquaredTo(attackLoc) > 10){
+                Direction moveDir = Pathfinder.pathBug(rc, attackLoc);
+                if(moveDir!= null && rc.canMove(moveDir)){
+                    rc.move(moveDir);
+                }
+            }
+        }
+    }
+
+    static RobotInfo findAttack(RobotController rc) throws GameActionException{
+        int maxValue = 0;
+        RobotInfo enemyToAttack = null;
+        for(RobotInfo enemy : enemies){
+            if(enemy.getType() != RobotType.HEADQUARTERS){
+                int attackValue = 0;
+
+                // weight value based on unit attacked
+                if(enemy.getType() == RobotType.LAUNCHER || enemy.getType()==RobotType.BOOSTER ||
+                        enemy.getType()==RobotType.DESTABILIZER){
+                    attackValue += 20 ;
+                } else if(enemy.getType() == RobotType.AMPLIFIER){
+                    attackValue +=15;
+                } else if(enemy.getType() == RobotType.CARRIER){
+                    attackValue += 10;
+                }
+
+                //add value for every hp point attacked
+                attackValue+= Math.min(enemy.getHealth(), 6);
+
+                //if you can kill the unit, add 10 value
+                if(enemy.getHealth() - 6 <= 0){
+                    attackValue+=10;
+                }
+
+                if(attackValue> maxValue){
+                    maxValue = attackValue;
+                    enemyToAttack = enemy;
+                }
+            }
+        }
+        return enemyToAttack;
+    }
+
+    static void pursue(RobotController rc) throws GameActionException{
+
+    }
+
+    static void explore(RobotController rc) throws GameActionException{
+
+        Direction dir = Helper.directions[Helper.rng.nextInt(Helper.directions.length)];
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+        }
+
+    }
+
+
+
+
+}
---
+++ src/DB_base_runaway/Pathfinder.java
@@ -0,0 +1,209 @@
+package DB_base;
+
+import battlecode.common.*;
+public strictfp class Pathfinder {
+
+    static boolean directBug;
+    static boolean rotatingBug;
+    static boolean wallLeft;
+    static Direction lastBugDir;
+    static int rotationCount;
+    static double lowestDist;
+
+    static double currentDist;
+
+    static MapLocation lastLocation;
+
+    static MapLocation lastTarget;
+
+    public static Direction pathBF(RobotController rc, MapLocation target) throws GameActionException {
+
+        if(!target.equals(lastTarget)){
+            directBug = false;
+            rotatingBug = false;
+        }
+        lastTarget=target;
+
+        currentDist = Math.sqrt(rc.getLocation().distanceSquaredTo(target));
+        if (directBug || (rotatingBug && currentDist>=lowestDist) ){
+            return pathBug(rc, target);
+        } else{
+            directBug = false;
+            rotatingBug = false;
+            Direction moveDir = BFPathing20.bfPathToTarget(rc, target);
+
+            if(moveDir ==null || moveDir == Direction.CENTER || rc.getLocation().add(moveDir).equals(lastLocation)){
+                directBug = true;
+
+                return pathBug(rc, target);
+            }
+
+
+            lastLocation = rc.getLocation();
+
+            return moveDir;
+        }
+
+    }
+
+
+    public static Direction pathGreedy(RobotController rc, MapLocation target)throws GameActionException {
+        if (directBug){
+            return pathBug(rc, target);
+        } else{
+            return pathGreedyDepth(rc,  target, 2);
+        }
+
+    }
+
+    public static Direction pathGreedyDepth(RobotController rc, MapLocation target, int depth)throws GameActionException {
+        if (directBug){
+            return pathBug(rc, target);
+        }
+        return pathBug(rc, target);
+
+    }
+
+    public static Direction pathBug(RobotController rc, MapLocation target)throws GameActionException {
+
+        boolean found = false;
+        currentDist = Math.sqrt(rc.getLocation().distanceSquaredTo(target));
+
+
+        if(rotatingBug){
+            rc.setIndicatorString("rotating "+target.toString() +
+                    " "+currentDist+" "+lowestDist +" "+lastBugDir+" wallleft:"+wallLeft);
+            if(currentDist < lowestDist){
+                rotatingBug = false;
+            }
+
+            if(wallLeft){
+                if(canMoveThrough(rc, lastBugDir.rotateLeft().rotateLeft(), rc.getLocation())
+                        || !rc.onTheMap(rc.getLocation().add(lastBugDir.rotateLeft().rotateLeft()))){
+                    rotatingBug = false;
+                    rc.setIndicatorString("canmovethrough");
+                } else{
+
+                    int count = 0;
+                    while( count <4){
+                        if(canMoveThrough(rc, lastBugDir.rotateLeft(), rc.getLocation())){
+                            Direction ans = lastBugDir.rotateLeft();
+                            lastBugDir = lastBugDir.rotateLeft().rotateLeft();
+                            return ans;
+                        } else if(canMoveThrough(rc, lastBugDir, rc.getLocation())){
+                            return lastBugDir;
+                        }
+                        lastBugDir = lastBugDir.rotateRight().rotateRight();
+                        count++;
+                    }
+
+                }
+
+            } else{
+                if(canMoveThrough(rc, lastBugDir.rotateRight().rotateRight(), rc.getLocation())
+                        || !rc.onTheMap(rc.getLocation().add(lastBugDir.rotateRight().rotateRight()))){
+                    rotatingBug = false;
+                } else{
+                    int count = 0;
+                    while( count <4){
+                        if(canMoveThrough(rc, lastBugDir.rotateRight(), rc.getLocation())){
+                            Direction ans = lastBugDir.rotateRight();
+                            lastBugDir = lastBugDir.rotateRight().rotateRight();
+                            return ans;
+                        } else if(canMoveThrough(rc, lastBugDir, rc.getLocation())){
+                            return lastBugDir;
+                        }
+                        lastBugDir = lastBugDir.rotateLeft().rotateLeft();
+                        count++;
+                    }
+                }
+            }
+
+
+            return Direction.CENTER;
+
+        } else{
+            rc.setIndicatorString("bug "+target.toString());
+
+            Direction moveDir = rc.getLocation().directionTo(target);
+            if(canMoveThrough(rc,moveDir, rc.getLocation())){
+                //rc.setIndicatorString("i can move to "+target.toString() + " "+moveDir.toString());
+                return moveDir;
+            } else{
+
+                Direction left = moveDir.rotateLeft();
+                Direction right = moveDir.rotateRight();
+
+                int i = 1;
+                while((!canMoveThrough(rc,left, rc.getLocation()) && !canMoveThrough(rc,right, rc.getLocation())) && i <=4){
+                    left = left.rotateLeft();
+                    right = right.rotateRight();
+                    i++;
+                }
+
+                if(i <=4){
+                    lowestDist = Math.sqrt(rc.getLocation().distanceSquaredTo(target));
+                    rotatingBug = true;
+                    directBug = false;
+                    rotationCount=0;
+                    if(canMoveThrough(rc,left, rc.getLocation())){
+
+                        wallLeft = false;
+
+                        if(left==Direction.NORTHEAST||left==Direction.NORTHWEST||left==Direction.SOUTHEAST||
+                                left==Direction.SOUTHWEST){
+                            lastBugDir= left.rotateRight();
+                        } else{
+                            lastBugDir= left;
+                        }
+                        rc.setIndicatorString("starting rotation "+target.toString() +" "+lastBugDir.toString()+
+                                " "+lowestDist);
+
+
+
+                        return left;
+                    } else{
+                        wallLeft = true;
+                        if(right==Direction.NORTHEAST||right==Direction.NORTHWEST||right==Direction.SOUTHEAST||
+                                right==Direction.SOUTHWEST){
+                            lastBugDir= right.rotateRight();
+                        } else{
+                            lastBugDir= right;
+                        }
+                        rc.setIndicatorString("starting rotation "+target.toString() +" "+lastBugDir.toString()+
+                                " "+lowestDist);
+                        return right;
+
+                    }
+
+                } else{
+                    return Direction.CENTER;
+                }
+            }
+        }
+
+    }
+
+    static boolean canMoveThrough(RobotController rc, Direction dir, MapLocation origin) throws GameActionException{
+
+        MapLocation dest = origin.add(dir);
+
+        if(rc.canSenseLocation(dest) && rc.sensePassability(dest) && !rc.isLocationOccupied(dest)){
+            MapInfo info = rc.senseMapInfo(dest);
+            Direction current = info.getCurrentDirection();
+            if(current != Direction.CENTER){
+
+                Direction opposite = dir.opposite();
+                Direction oppositeLeft = opposite.rotateLeft();
+                Direction oppositeRight = opposite.rotateRight();
+                if(current == opposite || current == oppositeLeft || current == oppositeRight){
+                    return false;
+                }
+                return true;
+            }
+            return true;
+        }
+        return false;
+
+    }
+}
